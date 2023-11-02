b4e176e93568465b34cd81234db87bc0e273ab17
jesusqc <jesusquirocampos@gmail.com>
2023-11-02 02:01:30 +0100

    💀

---
+++ SoftLockFix/Class1.cs
@@ -0,0 +1,15 @@
+﻿using PluginAPI.Core.Attributes;
+using ServerOutput;
+
+namespace SoftLockFix;
+
+internal class EntryPoint
+{
+    [PluginEntryPoint("SoftLockFix", "1.0.0.0", "Restarts the server each round.", "Jesus-QC")]
+    private void Init()
+    {
+        ServerLogs.AddLog(ServerLogs.Modules.Administrative, "[SoftLockFix] Restarting server next round.", ServerLogs.ServerLogType.RemoteAdminActivity_GameChanging);
+        ServerStatic.StopNextRound = ServerStatic.NextRoundAction.Restart;
+        ServerConsole.AddOutputEntry(default(ExitActionRestartEntry));
+    }
+}
